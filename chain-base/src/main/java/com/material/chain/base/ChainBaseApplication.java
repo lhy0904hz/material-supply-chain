73817060f7208c545d7143fd9fcc86e076ed7020
ziv <liu.haoyang@xyb2b.com>
2023-11-21 11:05:20 +0800

    chain-base

---
+++ chain-base/src/main/java/com/material/chain/base/ChainBaseApplication.java
@@ -0,0 +1,13 @@
+package com.material.chain.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainBaseApplication.class, args);
+    }
+
+}
---
+++ chain-base/src/test/java/com/material/chain/base/ChainBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.material.chain.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
